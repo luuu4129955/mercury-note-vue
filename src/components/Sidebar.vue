d004063b7280e136f5cc3690a3ba06b6c7795d38
crystalpeng <crystalpeng1998@gmail.com>
2022-01-13 21:02:57 +0800

    Sidebar 使用less写样式

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -14,40 +14,41 @@
 
 <script>
 import Avatar from "./Avatar";
+
 export default {
   components: {Avatar}
 }
 </script>
-<style scoped>
-#sidebar{
-position: relative;
+<style lang="less" scoped>
+#sidebar {
+  position: relative;
   width: 56px;
-text-align: center;
-  background-color: #2c333c;
-}
-
-.icons {
-  margin-top: 15px;
-}
-
-.icons a {
-  padding: 6px 0;
-  display: block;
-}
-
-.icons .router-link-active {
-  background-color: #5e6266;
-}
-
-.logout {
-  position: absolute;
-  bottom: 20px;
-  width: 100%;
   text-align: center;
-  cursor: pointer;
-}
+  background-color: #2c333c;
 
-.iconfont {
-  color: #fff;
+  .icons {
+    margin-top: 15px;
+
+    a {
+      padding: 6px 0;
+      display: block;
+    }
+
+    .router-link-active {
+      background-color: #5e6266;
+    }
+  }
+
+  .logout {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .iconfont {
+    color: #fff;
+  }
 }
 </style>
