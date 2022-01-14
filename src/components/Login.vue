93687068fca16823898d16ae62120ba6ca790407
crystalpeng <crystalpeng1998@gmail.com>
2022-01-14 23:03:53 +0800

    Login组件给密码输入框绑定键盘事件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,14 +8,14 @@
             <h3 @click="showRegister">创建账户</h3>
             <div v-show="isShowRegister" class="register">
               <input type="text" v-model="register.username" placeholder="用户名">
-              <input type="password" v-model="register.password" placeholder="密码">
+              <input type="password" v-model="register.password" @keyup.enter="onCheck(register)" placeholder="密码">
               <p :class="{error:register.isError}">{{ register.notice }}</p>
               <div class="button" @click="onCheck(register)">创建账号</div>
             </div>
             <h3 @click="showLogin">登录</h3>
             <div v-show="isShowLogin" class="login">
               <input type="text" v-model="login.username" placeholder="输入用户名">
-              <input type="password" v-model="login.password" placeholder="密码">
+              <input type="password" v-model="login.password" @keyup.enter="onCheck(login)" placeholder="密码">
               <p :class="{error:login.isError}">{{ login.notice }}</p>
               <div class="button" @click="onCheck(login)"> 登录</div>
             </div>
