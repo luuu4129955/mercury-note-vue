bdb610bdd35266230fd6dd1e075334e6e7bb469a
crystalpeng <crystalpeng1998@gmail.com>
2022-01-16 23:16:20 +0800

    重构 auth的登录注册接口

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,24 +1,18 @@
 import request from "../helpers/request";
 
-const URL={
-  REGISTER:'/auth/register',
-  LOGIN:'/auth/login',
-  LOGOUT:'auth/logout',
-  GET_INFO:'/auth'
+const URL = {
+  LOGOUT: 'auth/logout',
+  GET_INFO: '/auth'
 }
 
 export default {
-  register({username,password}){
-    return request(URL.REGISTER,'POST',{username,password})
+  loginOrRegister(obj, {username, password}) {
+    return request(`/auth/${obj.path}`, 'POST', {username, password})
   },
-  login({username,password}) {
-    return request(URL.LOGIN, 'POST', {username, password})
-  },
-
   logout() {
     return request(URL.LOGOUT)
   },
-  getInfo(){
+  getInfo() {
     return request(URL.GET_INFO)
   }
 
