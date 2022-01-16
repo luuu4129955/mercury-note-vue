10a42ef48c74ace55fea1ef82492357b5c1f7214
crystalpeng <crystalpeng1998@gmail.com>
2022-01-16 23:46:05 +0800

    Dev和Prod中 实现baseURL切换

---
+++ build/mock.config.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
+const mockBaseURL = 'http://localhost:3000'
+const realBaSeURL = 'https://note-server.hunger-valley.com'
+
+exports.config = function ({isDev = true} = {isDev: true}) {
+  let fileTxt = `
+  module.exports={
+    baseURL:'${isDev ? mockBaseURL : realBaSeURL}'
+  }
+  `
+  fs.writeFileSync(path.join(__dirname,'../src/helpers/config-baseURL.js'),fileTxt)
+}
+
