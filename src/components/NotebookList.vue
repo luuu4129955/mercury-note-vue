61927d27d98a8dc13b416c224d6a35dc99cb097e
crystalpeng <crystalpeng1998@gmail.com>
2022-01-21 23:33:02 +0800

    基本完成笔记本列表的样式

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class="detail" id="notebook-list">
+    <header>
+      <a href="#" class="btn" @click.prevent="onCreate"><i class="iconfont icon-plus"></i> 新建笔记本</a>
+    </header>
+    <main>
+      <div class="layout">
+        <h3>笔记本列表()</h3>
+        <div class="book-list">
+<!--          <router-link v-for="notebook in notebooks" to="/note/1" class="notebook">-->
+<!--            <div>-->
+<!--              <span class="iconfont icon-notebook"></span> {{notebook.title}}-->
+<!--              <span>{{notebook.noteCounts}}</span>-->
+<!--              <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>-->
+<!--              <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>-->
+<!--              <span class="date">{{notebook.friendlyCreatedAt}}</span>-->
+<!--            </div>-->
+<!--          </router-link>-->
+          <div>
+            <span class="iconfont icon-notebook"></span> 标题
+            <span>笔记数量</span>
+            <span class="action" @click.stop.prevent="onEdit()">编辑</span>
+            <span class="action" @click.stop.prevent="onDelete()">删除</span>
+            <span class="date">创建时间</span>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
   </div>
 </template>
 
@@ -20,12 +47,21 @@ export default {
         this.$router.push({path:'/login'})
       }
     })
+  },
+  methods:{
+    onCreate(){
+      console.log('create');
+    },
+    onEdit(){
+      console.log('edit');
+    },
+    onDelete(){
+      console.log('delete');
+    }
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  color: blue;
-}
+<style lang="less" scoped>
+@import url("../assets/css/notebook-list.less");
 </style>
